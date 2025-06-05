fc8d03ed90f51633db9ffaca8c548425c8bb5c53
Oliver-Form <oliverform@proton.me>
2025-06-05 11:17:53 +0100

    rearrangned html for mobiles

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -45,7 +45,7 @@ async fn main() {
     println!("Server listening on 127.0.0.1:8080");
     // Start the server
     warp::serve(routes)
-        .run(([127, 0, 0, 1], 8080))
+        .run(([0, 0, 0, 0], 8080))
         .await;
 }
 async fn handle_connection(
